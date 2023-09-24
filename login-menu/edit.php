3bdddc2edbb69c387109c71bf94438121ca8fe48
PhitahBinit <139530103+PhitahBinit@users.noreply.github.com>
2023-09-24 19:41:37 +0800

    Update edit.php

--- login-menu/edit.php
+++ login-menu/edit.php
@@ -367,13 +367,13 @@ body{
             }
             document.getElementById('age').value = age;
         });
-        <script>
+   
         function toggleMenu() {
             const menu = document.querySelector(".menu-links");
             const icon = document.querySelector(".hamburger-icon");
             menu.classList.toggle("open");
             icon.classList.toggle("open");
         }
-    </script>
+   
     </script>
 </body>
